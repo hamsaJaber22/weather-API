81e4b7e9e93a2637c35c937912a057834d15ee8f
unknown <you@example.com>
2022-01-02 09:25:46 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+let cityName = document.getElementById("cityName");
+let weather = document.getElementById("weather");
+
+let result = ``;
+let results = ``;
+
+let c = prompt("enter the city name ");
+let myHttp = new XMLHttpRequest();
+const url = `http://api.weatherapi.com/v1/current.json?key=3cfcb3fef8ce4b3e8b3201618212612&q=${c}&aqi=no`;
+myHttp.onreadystatechange = function() {
+    if (myHttp.readyState == 4 && myHttp.status == 200) {
+        let data = JSON.parse(myHttp.responseText);
+
+        result += `
+        <div class="two">
+           <h2 class = "cityName" > city name: ${data.location.name}</h2>      
+           <img alt="image" src="${data.current.condition.icon}"/> 
+        </div>
+`;
+
+        results += `<h3 class = "weathers"> temp c : ${data.current.temp_c}</h3> 
+                    <h3 class="weathers"> temp f : ${data.current.temp_f} </h3> 
+                    <h3 class="weathers"> cloud : ${data.current.cloud} </h3> 
+                    <h3 class="weathers"> description : ${data.current.condition.text} </h3>    
+                    <h3 class = "time" >${data.location.localtime}</h3>                 
+            `;
+    }
+
+    cityName.innerHTML = result;
+    weather.innerHTML = results;
+
+}
+myHttp.open("GET", url);
+myHttp.send();
